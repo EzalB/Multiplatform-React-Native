1b71c60878b6e1b047ae9178e47720734eadb3f0
Rusheel <rusheel1309@gmail.com>
2020-07-30 17:33:59 +0530

    Persist Redux updated

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -7,11 +7,11 @@ import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
 import { persistStore, persistCombineReducers } from 'redux-persist';
-import storage from 'redux-persist/es/storage';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const config = {
     key: 'root',
-    storage,
+    storage: AsyncStorage,
     debug: true
 }
 
